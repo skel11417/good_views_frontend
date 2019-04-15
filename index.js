6f38040b0ba198b8f5f61ae1e23b97e2795aac30
seanlucius <seanlucius22@icloud.com>
2019-04-15 18:02:34 -0400

    add frontend styling, poster display

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
-document.addEventListener('DOMContentLoaded', initialize)
+document.addEventListener("DOMContentLoaded", initialize);
 
-
-function initialize(){
-  const form =document.querySelector('form')
-  form.addEventListener('submit', getMovies)
+function initialize() {
+  const form = document.querySelector("form");
+  form.addEventListener("submit", getMovies);
 }
 
-function getMovies(event){
-  event.preventDefault()
-  const title = event.target.title.value
-  const url=`http://localhost:3000/movies/?title=${title}`
-  fetch(url).then(resp => resp.json()).then(json => console.log(json))
+function getMovies(event) {
+  event.preventDefault();
+  const title = event.target.title.value;
+  const url = `http://localhost:3000/movies/?title=${title}`;
+  fetch(url)
+    .then(resp => resp.json())
+    .then(renderPoster);
+  event.target.reset();
+}
+
+function renderPoster(movie) {
+  let posterDiv = document.querySelector("#poster");
+  let image = document.createElement("img");
+  posterDiv.innerHTML = "";
+  image.src = movie.poster;
+
+  posterDiv.appendChild(image);
 }
