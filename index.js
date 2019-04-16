5d40f3f940d7eb522492b0b85560cfbb05655648
Sean <shonkelli@gmail.com>
2019-04-16 11:04:42 -0400

    made a mess of the front end

--- index.js
+++ index.js
@@ -2,24 +2,45 @@ document.addEventListener("DOMContentLoaded", initialize);
 
 function initialize() {
   const form = document.querySelector("form");
-  form.addEventListener("submit", getMovies);
+  form.addEventListener("submit", searchMovies);
 }
 
-function getMovies(event) {
+function searchMovies(event) {
   event.preventDefault();
   const title = event.target.title.value;
   const url = `http://localhost:3000/movies/?title=${title}`;
   fetch(url)
     .then(resp => resp.json())
-    .then(renderPoster);
+    .then(renderSearchPosters);
   event.target.reset();
 }
 
-function renderPoster(movie) {
-  let posterDiv = document.querySelector("#poster");
-  let image = document.createElement("img");
+function renderSearchPosters(omdbObj){
+  let posterDiv = document.querySelector("#posters");
   posterDiv.innerHTML = "";
-  image.src = movie.poster;
-
-  posterDiv.appendChild(image);
+  let movies = omdbObj["Search"]
+  movies.forEach(renderSearchPoster)
 }
+
+function renderSearchPoster(movieObj){
+  let posterContainer = document.querySelector("#posters");
+
+  // let link = document.createElement('a');
+  // link.dataset.lightbox = movieObj["Poster"]
+  // link.pathname = movieObj["Poster"]
+  // '<a href="images/image-1.jpg" data-lightbox="image-1" data-title="My caption">Image #1</a>'
+
+  // let image = document.createElement("img");
+  // image.dataset.id = movieObj["imdbID"]
+  // image.src = movieObj["Poster"];
+  // image.alt = movieObj["Title"]
+  posterContainer.appendChild(link);
+}
+
+// function renderPoster(movie) {
+//   let posterDiv = document.querySelector("#poster");
+//   let image = document.createElement("img");
+//   posterDiv.innerHTML = "";
+//   image.src = movie.poster;
+//   posterDiv.appendChild(image);
+// }
