e50aef4b6e46625580fae9ade86d511392d6ec4a
Sean <shonkelli@gmail.com>
2019-04-18 11:19:06 -0400

    created recent reviews show page and some navbar functionality

--- index.js
+++ index.js
@@ -2,12 +2,86 @@ document.addEventListener("DOMContentLoaded", initialize);
 
 function initialize() {
   const form = document.querySelector("form");
+  const homeLink = document.querySelector('#home-link')
+  homeLink.addEventListener('click', loadAllRecentReviews)
+  const myMoviesLink = document.querySelector("#my-movies-link")
+  myMoviesLink.addEventListener('click', loadRecentUserReviews)
   form.addEventListener("submit", searchMovies);
 
-  loadRecent();
+  loadAllRecentReviews();
 }
 
-function loadRecent() {
+function loadAllRecentReviews(){
+  let main = document.querySelector('#main')
+  main.innerHTML = "<div style='width:800px'><h2>Recent Reviews</h2></div>"
+  fetch("http://localhost:3000/reviews")
+    .then(resp => resp.json())
+    .then(movies => {
+      movies.forEach(renderReview);
+    });
+}
+
+function renderReview(review) {
+  let main = document.querySelector('#main')
+  let reviewDiv = document.createElement('div')
+  reviewDiv.className = "review-card"
+  reviewDiv.id = review.id
+  reviewDiv.dataset.id = review.movie.imdb_id
+  reviewDiv.addEventListener('click', movieClickHandler)
+
+  // Poster
+  let poster = document.createElement('img')
+  poster.src = review.movie.poster
+
+  // Title
+  let title = document.createElement('p')
+  title.innerText = review.movie.title
+
+  // Review Content
+  let reviewContent = document.createElement('p')
+  reviewContent.innerText = review.content
+
+  // Rating
+  let ratingDiv = document.createElement("div");
+  ratingDiv.classList.add("rating");
+
+  let star1 = document.createElement("span");
+  let star2 = document.createElement("span");
+  let star3 = document.createElement("span");
+  let star4 = document.createElement("span");
+  let star5 = document.createElement("span");
+
+  // Stars
+  star1.dataset.id = "1";
+  star1.className = "fa fa-star";
+  star2.dataset.id = "2";
+  star2.className = "fa fa-star";
+  star3.dataset.id = "3";
+  star3.className = "fa fa-star";
+  star4.dataset.id = "4";
+  star4.className = "fa fa-star";
+  star5.dataset.id = "5";
+  star5.className = "fa fa-star";
+
+  ratingDiv.append(star1, star2, star3, star4, star5);
+  reviewDiv.append(poster, title, ratingDiv, reviewContent)
+
+  main.appendChild(reviewDiv)
+
+  let rating = review.rating;
+  let stars = Array.prototype.slice
+  .call(reviewDiv.querySelectorAll(".fa"))
+  .slice(0, rating);
+  stars.forEach(star => {
+    star.className += " checked";
+  });
+}
+
+
+function loadRecentUserReviews(event) {
+  event.preventDefault()
+  let mainDiv = document.querySelector("#main");
+  mainDiv.innerHTML = ""
   fetch("http://localhost:3000/users/?recent=true")
     .then(resp => resp.json())
     .then(movies => {
@@ -20,7 +94,7 @@ function renderRecentMovie(movie) {
   let image = document.createElement("img");
   image.src = movie.poster;
   image.dataset.id = movie.imdb_id;
-  image.addEventListener("click", posterClickHandler);
+  image.addEventListener("click", movieClickHandler);
   mainDiv.appendChild(image);
 }
 
@@ -44,6 +118,7 @@ function renderSearchPoster(movieObj) {
   let mainDiv = document.querySelector("#main");
   let card = document.createElement("article");
   card.className = "card";
+  card.dataset.id = movieObj["imdbID"];
   mainDiv.appendChild(card);
 
   let thumbnail = document.createElement('figure')
@@ -52,7 +127,6 @@ function renderSearchPoster(movieObj) {
   let image = document.createElement("img");
   image.src = movieObj["Poster"];
   image.alt = movieObj["Title"];
-  image.dataset.id = movieObj["imdbID"];
 
   thumbnail.appendChild(image)
 
@@ -61,15 +135,15 @@ function renderSearchPoster(movieObj) {
 
   let movieTitle = document.createElement('p')
   movieTitle.innerText = movieObj["Title"]
-  
+
   cardContent.appendChild(movieTitle)
   card.append(thumbnail, cardContent)
-  image.addEventListener("click", posterClickHandler);
+  card.addEventListener("click", movieClickHandler);
 }
 
-function posterClickHandler(event) {
+function movieClickHandler(event) {
   event.preventDefault();
-  let imdbID = event.target.dataset.id;
+  let imdbID = event.currentTarget.dataset.id;
   const url = `http://localhost:3000/movies/${imdbID}`;
   fetch(url)
     .then(resp => resp.json())
@@ -229,7 +303,7 @@ function rateMovie(event) {
       document.querySelectorAll(".fa").forEach(star => {
         star.removeEventListener("click", rateMovie);
       });
-    });
+  });
   renderReviewForm();
 }
 
